a31241b7da85f199c901ea0cb8d4bc85c8ce8996
Ranit Bose <59643631+boseranit@users.noreply.github.com>
2026-07-12 13:16:11 +1000

    Separate LaTeX editor configuration

--- home/.config/nvim/lua/plugins/editor.lua
+++ home/.config/nvim/lua/plugins/editor.lua
@@ -39,27 +39,4 @@ return {
   },
   { "tpope/vim-sleuth" },
   { "karb94/neoscroll.nvim", opts = {} },
-  {
-    "lervag/vimtex",
-    lazy = false,
-    init = function()
-      vim.g.tex_flavor = "latex"
-      vim.g.vimtex_compiler_latexmk = {
-        out_dir = "build",
-        options = {
-          "-shell-escape",
-          "-verbose",
-          "-file-line-error",
-          "-interaction=nonstopmode",
-          "-synctex=1",
-        },
-      }
-      vim.g.vimtex_view_general_viewer = "SumatraPDF"
-      vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
-      vim.g.tex_conceal = "abdmg"
-    end,
-    config = function()
-      vim.api.nvim_set_hl(0, "Conceal", { bg = "NONE" })
-    end,
-  },
 }
